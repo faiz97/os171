79dcbffaedd8da42a30959e73c34668f5720a833
MAULA FAIZ ADLI - 1506722853 <maula.faiz@badak.cs.ui.ac.id>
2017-02-21 13:04:34 +0700

    correcting my week02

--- week02/work02/exercise.c
+++ week02/work02/exercise.c
@@ -8,13 +8,11 @@
 #include <stdio.h>
 void main() {
  int input = 5; //define me!
- int ii,jj;
+ int ii;
  int THE_PRODUCT = 0;
  // TO DO implement me!!
  for(ii=1;ii <= input;ii++){
-	for(jj=1;jj <= LOOP;jj++){
-	 THE_PRODUCT++;
-	}
+  THE_PRODUCT += LOOP;
  }
  printf("%d times %d equals %d\n",input,LOOP,THE_PRODUCT);
 }
